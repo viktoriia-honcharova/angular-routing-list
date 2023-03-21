8f249dd5f966576cf4a37e96506d2d1746e8e95c
viktoriia-honcharova <vika.honcharova.619853@gmail.com>
2023-03-21 10:14:20 +0200

    Generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { WorksComponent } from './components/works/works.component';
+import { CenzorComponent } from './components/works/cenzor/cenzor.component';
+import { UserListComponent } from './components/works/user-list/user-list.component';
+import { TaskListComponent } from './components/works/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorksComponent,
+    CenzorComponent,
+    UserListComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/works/cenzor/cenzor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cenzor',
+  templateUrl: './cenzor.component.html',
+  styleUrls: ['./cenzor.component.css']
+})
+export class CenzorComponent {
+
+}
