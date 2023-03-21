836e818cb0489d13c4037e9f49903bd61aaeef8b
viktoriia-honcharova <vika.honcharova.619853@gmail.com>
2023-03-21 18:57:38 +0200

    Added cenzor task code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,13 +17,10 @@ import { TaskListComponent } from './components/works/task-list/task-list.compon
     WorksComponent,
     CenzorComponent,
     UserListComponent,
-    TaskListComponent
+    TaskListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
