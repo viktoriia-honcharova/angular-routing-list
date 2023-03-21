032167a96f90fa2b49f3ded75c0fcbcd21848b24
viktoriia-honcharova <vika.honcharova.619853@gmail.com>
2023-03-21 12:12:42 +0200

    Created home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { WorksComponent } from './components/works/works.component';
+import { CenzorComponent } from './components/works/cenzor/cenzor.component';
+import { UserListComponent } from './components/works/user-list/user-list.component';
+import { TaskListComponent } from './components/works/task-list/task-list.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'works/:project', component: WorksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
